30e3347785bc4dac2c0739a331ce30ada75a77b1
itachicodeslikecrazy <veeralnarang999@gmail.com>
2026-01-23 20:45:38 +0530

    Fixed profile controllers, forgot to use api handler

--- apps/api/src/modules/profile/profile.controller.ts
+++ apps/api/src/modules/profile/profile.controller.ts
@@ -5,54 +5,59 @@ import type {
   UpdateProfileFormData,
   UpdateUsernameFormData,
 } from "@repo/shared-types";
+import { apiHandler } from "../../utils/apiHandler";
 
-const getSelfProfile = async (req: Request, res: Response) => {
+const getSelfProfile = apiHandler(async (req: Request, res: Response) => {
   const userId = req.user?.id;
   const profile = await profileService.getProfileByUserId(userId as string);
   successResponse(res, "Profile fetched successfully", profile);
-};
+});
 
-const getProfileByUsername = async (req: Request, res: Response) => {
+const getProfileByUsername = apiHandler(async (req: Request, res: Response) => {
   const username = req.params["username"];
   const profile = await profileService.getProfileByUsername(username as string);
   successResponse(res, "Profile fetched successfully", profile);
-};
+});
 
-const updateUserProfile = async (
-  req: Request<{}, {}, UpdateProfileFormData>,
-  res: Response,
-) => {
-  const userId = req.user?.id;
-  const data = req.validatedBody as UpdateProfileFormData;
-  const avatarFile = req.file || null;
+const updateUserProfile = apiHandler(
+  async (req: Request<{}, {}, UpdateProfileFormData>, res: Response) => {
+    const userId = req.user?.id;
+    const data = req.validatedBody as UpdateProfileFormData;
+    const avatarFile = req.file || null;
 
-  const profile = await profileService.updateUserProfile(
-    userId as string,
-    data,
-    avatarFile,
-  );
+    const profile = await profileService.updateUserProfile(
+      userId as string,
+      data,
+      avatarFile,
+    );
 
-  successResponse(res, "Profile Updated Successfully", profile);
-};
+    successResponse(res, "Profile Updated Successfully", profile);
+  },
+);
 
-const checkUsernameAvailability = async (req: Request, res: Response) => {
-  const username = req.query["username"] as string;
-  const isAvailable = await profileService.checkUsernameAvailability(username);
-  successResponse(res, "Username availability checked successfully", {
-    isAvailable,
-  });
-};
+const checkUsernameAvailability = apiHandler(
+  async (req: Request, res: Response) => {
+    const username = req.query["username"] as string;
+    const isAvailable =
+      await profileService.checkUsernameAvailability(username);
+    successResponse(res, "Username availability checked successfully", {
+      isAvailable,
+    });
+  },
+);
 
-export const updateUsername = async (req: Request, res: Response) => {
-  const userId = req.user?.id;
-  const data = req.validatedBody as UpdateUsernameFormData;
+export const updateUsername = apiHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user?.id;
+    const data = req.validatedBody as UpdateUsernameFormData;
 
-  const updatedProfie = await profileService.updateUsername(
-    userId as string,
-    data.username,
-  );
-  successResponse(res, "Username changed successfully", updatedProfie);
-};
+    const updatedProfie = await profileService.updateUsername(
+      userId as string,
+      data.username,
+    );
+    successResponse(res, "Username changed successfully", updatedProfie);
+  },
+);
 export default {
   getSelfProfile,
   getProfileByUsername,
--- packages/shared-types/src/user/user.types.ts
+++ packages/shared-types/src/user/user.types.ts
@@ -1,7 +1,16 @@
+import { Profile } from "../profile/profile.types";
+
 export type PublicUser = {
   id: string;
   firstName: string;
   lastName?: string | null;
   username: string;
   email: string;
+};
+
+export type UserWithProfile = {
+  id: string;
+  email: string;
+  isVerified: boolean;
+  profile?: Profile | null;
 };
